a92f63d257f85df25a6e0325f3c271ba49b2caee
Grant Schwiegeraht <schwiegerg1@mymail.nku.edu>
2022-03-16 06:31:50 -0700

    Initial Commit

---
+++ DinoList/DinoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  DinoListApp.swift
+//  DinoList
+//
+//  Created by student on 3/16/22.
+//
+
+import SwiftUI
+
+@main
+struct DinoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
